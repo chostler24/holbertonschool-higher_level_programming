4fca42841d31e3dcdfd5fe289eab4839398d8a49
chostler24 <connor.hostler@gmail.com>
2022-09-01 15:20:01 -0700

    And heres the rest

---
+++ 0x02-python-data_structures/9-max_integer.py
@@ -0,0 +1,10 @@
+def max_integer(my_list=[]):
+    if len(my_list) == 0:
+        return (None)
+
+    me = my_list[0]
+    for x in range(len(my_list)):
+        if my_list[x] > me:
+            me = my_list[x]
+    
+    return (me)
