ab7d50a272ab41bbdb859acf0a2403e36c0aa343
chostler24 <connor.hostler@gmail.com>
2022-09-01 15:18:41 -0700

    six fix

--- 0x02-python-data_structures/6-print_matrix_integer.py
+++ 0x02-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for row in matrix:
-        for col in matrix:
-            print("{:d}".format(col), end=" " if col is not row[-1] else "")
-    print()
+    for x in matrix:
+        print(' '.join("{:d}".format(i) for i in x))
