00ec4b21f357855e86adfa56c0dde72f4efff589
chostler24 <connor.hostler@gmail.com>
2022-09-07 10:51:18 -0700

    adding task 0

---
+++ 0x04-python-classes/0-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Class defined as Square
+
+Classes:
+    Square
+"""
+
+class Square:
+    """A class that represents a Square"""
+    def __init__(self, size=0):
+        """Attributes for the square object
+        
+        Args:
+            size (int): size of square
+
+        """
+        self.__size = size
