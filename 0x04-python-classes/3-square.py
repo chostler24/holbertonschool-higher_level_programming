f622b30e8c0f0842874e3234d5363f3c3a3095ed
chostler24 <connor.hostler@gmail.com>
2022-09-07 14:17:42 -0700

    Property fix

--- 0x04-python-classes/3-square.py
+++ 0x04-python-classes/3-square.py
@@ -7,7 +7,14 @@ class Square:
     """This class is defined as a Square"""
     def __init__(self, size=0):
         """Attributes for the class"""
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
         self.__size = size
+
+    def area(self):
+        return self.__size ** 2
 
     @property
     def size(self):
@@ -19,6 +26,4 @@ class Square:
             raise TypeError("size must be an integer")
         if value < 0:
             raise ValueError("size must be >= 0")
-
-    def area(self):
-        return self.__size ** 2
+        self.__size = value
