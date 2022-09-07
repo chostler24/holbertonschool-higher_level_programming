87e9f3cd93184481dea514c3621f535f22842992
chostler24 <connor.hostler@gmail.com>
2022-09-07 13:07:28 -0700

    Removed whitespace

--- 0x04-python-classes/3-square.py
+++ 0x04-python-classes/3-square.py
@@ -12,13 +12,13 @@ class Square:
     @property
     def size(self):
         return self.__size
-    
+
     @size.setter
     def size(self, value):
         if type(value) is not int:
             raise TypeError("size must be an integer")
         if value < 0:
             raise ValueError("size must be >= 0")
-    
+
     def area(self):
         return self.__size ** 2
