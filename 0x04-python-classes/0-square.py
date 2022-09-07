86cbc3d1755012f0f5ca26d864e0d4ff46f6647f
chostler24 <connor.hostler@gmail.com>
2022-09-07 10:55:45 -0700

    fixed documentation

--- 0x04-python-classes/0-square.py
+++ 0x04-python-classes/0-square.py
@@ -6,11 +6,14 @@ Classes:
     Square
 """
 
+
 class Square:
+
     """A class that represents a Square"""
+
     def __init__(self, size=0):
         """Attributes for the square object
-        
+
         Args:
             size (int): size of square
 
