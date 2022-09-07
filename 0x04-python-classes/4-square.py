e4a5d137bc62fd0e7add84342e26176f176ea63f
chostler24 <connor.hostler@gmail.com>
2022-09-07 14:31:02 -0700

    Adding task 4

---
+++ 0x04-python-classes/4-square.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+
+"""Class: Square"""
+
+
+class Square:
+    """This class is defined as a Square"""
+    def __init__(self, size=0):
+        """Attributes for the class"""
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        return self.__size ** 2
+
+    def my_print(self):
+        if self.__size == 0:
+            print("")
+        else:
+            for i in range(self.__size):
+                for x in range(self.__size):
+                    print("#", end="")
+                print("")
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
