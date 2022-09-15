512712108432ab08c7db990685c5f8a7c2ed3ba4
chostler24 <connor.hostler@gmail.com>
2022-09-15 11:46:39 -0700

    Adding task 2

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""2-is_same_class.py module"""
+
+
+def is_same_class(obj, a_class):
+    """
+    returns true if object is exactly an instance
+    of specified class, otherwise false
+    """
+    return isinstance(type(obj), a_class)
