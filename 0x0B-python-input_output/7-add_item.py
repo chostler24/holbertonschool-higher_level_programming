36360eca475cc75aa9947b1ce5874cc7e8706bff
chostler24 <connor.hostler@gmail.com>
2022-09-21 13:44:44 -0700

    fixed try/except statements

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -2,5 +2,14 @@
 """7-add_item.py module"""
 
 
+from sys import argv
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+
+try:
+    json_make = load_from_json_file("add_item.json")
+except:
+    json_make = []
+
+save_to_json_file(json_make + argv[1:], "add_item.json")
