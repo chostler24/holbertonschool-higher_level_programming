31234f8feb30ac982466a9da0e1247257545f457
chostler24 <connor.hostler@gmail.com>
2022-09-22 13:06:38 -0700

    task 1

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""base.py module"""
+
+
+class Base:
+    """class defining Base"""
+    __nb_objects = 0
+    def __init__(self, id=None):
+        """attributes"""
+        if id is None:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
+        else:
+            self.id = id
