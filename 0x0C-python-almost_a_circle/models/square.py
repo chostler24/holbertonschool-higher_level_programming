c68f9f24178f0b1b7b2ef18bd51589c89f4b0700
chostler24 <connor.hostler@gmail.com>
2022-09-23 07:44:56 -0700

    task 12

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -22,5 +22,27 @@ class Square(Rectangle):
         """string rep"""
         return "[{}] ({}) {}/{} - {}".format(
             type(self).__name__, self.id, self.x,
-            self.y, self.height
+            self.y, self.width
         )
+
+    def update(self, *args, **kwargs):
+        """updating arguments"""
+        i = 0
+        for arg in args:
+            i += 1
+            if i == 1:
+                self.id = arg
+            if i == 2:
+                self.width = arg
+            if i == 3:
+                self.y = arg
+
+        for key, value in kwargs.items():
+            if key == "id":
+                self.id = value
+            if key == "size":
+                self.width = value
+            if key == "x":
+                self.x = value
+            if key == "y":
+                self.y = value
