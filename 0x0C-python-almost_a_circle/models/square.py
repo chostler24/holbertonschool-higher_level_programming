42b61ce613eb987274a469077aec6186579e3705
chostler24 <connor.hostler@gmail.com>
2022-09-23 07:37:58 -0700

    task 11

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -8,6 +8,16 @@ class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         super().__init__(size, size, x, y, id)
 
+    @property
+    def size(self):
+        """size getter"""
+        return self.width
+
+    @size.setter
+    def size(self, size):
+        """size setter"""
+        self.width = size
+
     def __str__(self):
         """string rep"""
         return "[{}] ({}) {}/{} - {}".format(
