54a80c32f3b21365ffb245eb0bea02590996de2b
chostler24 <connor.hostler@gmail.com>
2022-09-22 17:37:42 -0700

    Adding string method to square

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -7,3 +7,10 @@ class Square(Rectangle):
     """class defining Square inherited from Rectangle"""
     def __init__(self, size, x=0, y=0, id=None):
         super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """string rep"""
+        return "[{}] ({}) {}/{} - {}".format(
+            type(self).__name__, self.id, self.x,
+            self.y, self.height
+        )
