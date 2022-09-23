66159f10462c776640a69589cb8167a0bf94b1b6
chostler24 <connor.hostler@gmail.com>
2022-09-23 07:53:19 -0700

    task 13

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -138,3 +138,11 @@ class Rectangle(Base):
                 self.__x = value
             if key == "y":
                 self.__y = value
+
+    def to_dictionary(self):
+        """returns dictionary representation"""
+        return {
+            "id": self.id, "width": self.__width,
+            "height": self.__height, "x": self.__x,
+            "y": self.__y
+        }
