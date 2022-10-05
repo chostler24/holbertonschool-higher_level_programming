5f042de00a2added1a0323ec7fc97e7160633a2e
chostler24 <connor.hostler@gmail.com>
2022-10-05 11:57:06 -0700

    changed syntax

--- 0x00-SQL_introduction/9-full_creation.sql
+++ 0x00-SQL_introduction/9-full_creation.sql
@@ -4,7 +4,8 @@ CREATE TABLE IF NOT EXISTS second_table (
     name VARCHAR(256),
     score INT
 );
-INSERT INTO TABLE second_table VALUES (1, "John", 10),
+INSERT INTO TABLE second_table
+VALUES (1, "John", 10),
 (2, "Alex", 3),
 (3, "Bob", 14),
-(4, "George", 8)
+(4, "George", 8);
