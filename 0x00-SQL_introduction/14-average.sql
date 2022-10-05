a22090a707d0f101bdc2239ef5ce7c4ef969a96d
chostler24 <connor.hostler@gmail.com>
2022-10-05 13:08:59 -0700

    trying diff method

--- 0x00-SQL_introduction/14-average.sql
+++ 0x00-SQL_introduction/14-average.sql
@@ -1,4 +1,2 @@
 -- computes score average of all records in second_table
-ALTER TABLE second_table
-ADD average FLOAT
-SELECT AVG(score) FROM second_table
+SELECT AVG(score) 'average' FROM second_table
