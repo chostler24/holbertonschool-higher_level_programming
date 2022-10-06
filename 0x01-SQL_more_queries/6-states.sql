068571ac5b84a76ceeb30b7c2a8ccc87e1e6bb02
chostler24 <connor.hostler@gmail.com>
2022-10-06 09:59:07 -0700

    task 6

---
+++ 0x01-SQL_more_queries/6-states.sql
@@ -0,0 +1,7 @@
+-- creates db hbtn_0d_usa and tbl states
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(256) NOT NULL
+);
+AUTO_INCREMENT = 1;
