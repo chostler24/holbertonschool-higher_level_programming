e82e16f9748ec77c62602ec42c4e18409a544187
chostler24 <connor.hostler@gmail.com>
2022-10-06 08:17:50 -0700

    added privileges statement

--- 0x01-SQL_more_queries/1-create_user.sql
+++ 0x01-SQL_more_queries/1-create_user.sql
@@ -1,2 +1,4 @@
 -- creates user_0d_1 with all privileges
-CREATE USER IF NOT EXISTS user_0d_1 IDENTIFIED BY 'user_0d_1_pwd';
+CREATE USER IF NOT EXISTS user_0d_1;
+GRANT ALL PRIVILEGES ON mysql TO user_0d_1;
+IDENTIFIED BY user_0d_1_pwd;
