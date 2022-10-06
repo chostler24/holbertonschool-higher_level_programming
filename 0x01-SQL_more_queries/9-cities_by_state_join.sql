9ccac16da40eb10d80fcf6a599b17c484a35c829
chostler24 <connor.hostler@gmail.com>
2022-10-06 11:21:23 -0700

    added order

--- 0x01-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x01-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,4 +1,5 @@
 -- list all cities contained in db hbtn_0d_usa
 SELECT cities.id, cities.name, states.name
 FROM cities
-INNER JOIN states ON cities.states_id=states.id;
+INNER JOIN states ON cities.states_id=states.id
+ORDER BY id ASC;
