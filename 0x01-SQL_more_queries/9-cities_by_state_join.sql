a933b646185d02c9d3e759df38a4b0ccb4de7592
chostler24 <connor.hostler@gmail.com>
2022-10-06 11:55:51 -0700

    trying agin

--- 0x01-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x01-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,4 @@
 -- list all cities contained in db hbtn_0d_usa
 SELECT cities.id, cities.name, states.name
 FROM cities
-INNER JOIN states ON cities.states_id=states.id
-ORDER BY id ASC;
+INNER JOIN states ON cities.states_id=states.id;
