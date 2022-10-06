af33fbd1a35cd65ecd40a9bd42b9307091f1cbaa
chostler24 <connor.hostler@gmail.com>
2022-10-06 11:57:21 -0700

    typo

--- 0x01-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x01-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,4 +1,4 @@
 -- list all cities contained in db hbtn_0d_usa
 SELECT cities.id, cities.name, states.name
 FROM cities
-INNER JOIN states ON cities.states_id=states.id;
+INNER JOIN states ON cities.state_id=states.id;
