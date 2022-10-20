f4ca52212588129da371b7870514737ef28998ae
chostler24 <connor.hostler@gmail.com>
2022-10-20 12:13:07 -0500

    query syntax

--- 0x0B-python-object_relational_mapping/0-select_states.py
+++ 0x0B-python-object_relational_mapping/0-select_states.py
@@ -13,7 +13,7 @@ def state_lister():
                          user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
 
     cur = db.cursor()
-    cur.execute("SELECT * FROM states ORDER by id")
+    cur.execute("SELECT * FROM states ORDER BY id")
     rows = cur.fetchall()
     for row in rows:
         print(row)
