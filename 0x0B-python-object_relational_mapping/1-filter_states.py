2e985296c8353ad383338c71a8838016c8b37d79
chostler24 <connor.hostler@gmail.com>
2022-10-20 13:16:18 -0500

    pep8

--- 0x0B-python-object_relational_mapping/1-filter_states.py
+++ 0x0B-python-object_relational_mapping/1-filter_states.py
@@ -11,16 +11,17 @@ def state_lister_n():
     """lists N states"""
     db = MySQLdb.connect(host="localhost", port=3306,
                          user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    
-    cur=db.cursor()
+
+    cur = db.cursor()
     cur.execute("SELECT * FROM states WHERE name LIKE BINARY '%N' \
                 ORDER BY id")
     rows = cur.fetchall()
     for row in rows:
         print(row)
-    
+
     cur.close()
     db.close()
+
 
 if __name__ == "__main__":
     state_lister_n()
