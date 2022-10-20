8d0d45c40d175b06a026930fe1c16042b7a912a3
chostler24 <connor.hostler@gmail.com>
2022-10-20 14:24:10 -0500

    changed query method

--- 0x0B-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0B-python-object_relational_mapping/4-cities_by_state.py
@@ -13,7 +13,8 @@ def city_lister():
                          user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
 
     cur = db.cursor()
-    cur.execute("SELECT * FROM cities ORDER BY id")
+    cur.execute("SELECT cities.id, cities.name, states.name \
+        FROM cities LEFT JOIN states ON cities.state_id = states.id")
     rows = cur.fetchall()
     for row in rows:
         print(row)
