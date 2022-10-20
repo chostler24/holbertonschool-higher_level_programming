d273b75d3c7e0b338462d781b9160fca303a1214
chostler24 <connor.hostler@gmail.com>
2022-10-20 11:33:59 -0500

    task 0

---
+++ 0x0B-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+0-select_states.py module
+Lists all states from the db hbtn_0e_0_usa
+"""
+import MySQLdb
+import sys
+
+def state_lister():
+    """function that lists states"""
+    db = MySQLdb.connect(host="localhost", port=3306,
+        user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    
+    cur = db.cursor()
+
+    cur.execute("SELECT * FROM states ORDER by id")
+
+    query = cur.fetchall()
+
+    for row in query:
+        print(row)
+
+    db.close()
+
+    cur.close()
+
+    if __name__ == "__main__":
+        state_lister()
