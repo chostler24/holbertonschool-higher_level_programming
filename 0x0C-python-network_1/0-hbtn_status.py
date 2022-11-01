430c6b711862e6d44cc40e561e530267980a19dd
chostler24 <connor.hostler@gmail.com>
2022-11-01 11:27:06 -0500

    adding task 0

---
+++ 0x0C-python-network_1/0-hbtn_status.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Script that fetches a URL"""
+import urllib.request
+
+
+if __name__ == "__main__":
+    r = urllib.request.Request('https://intranet.hbtn.io/status')
+    with urllib.request.urlopen(r) as response:
+        page = response.read()
+        print('Body response:')
+        print('\t- type: {}'.format(type(page)))
+        print('\t- content: {}'.format(page))
+        print('\t- utf8 content: {}'.format(page.decode('utf8')))
