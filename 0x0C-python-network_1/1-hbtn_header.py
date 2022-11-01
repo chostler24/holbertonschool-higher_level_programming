06876a8e2177358301676c47dcd0479a6b2d875d
chostler24 <connor.hostler@gmail.com>
2022-11-01 11:42:32 -0500

    adding task 1

---
+++ 0x0C-python-network_1/1-hbtn_header.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Script that takes in a URL, sends request
+to the URL, and displays value of X-Request-Id
+variable in header of response.
+"""
+import urllib.request
+import sys
+
+
+if __name__ == "__main__":
+    r = urllib.request.Request(sys.argv[1])
+    with urllib.request.urlopen(r) as response:
+        print(repsonse.info()['X-Request-Id'])
