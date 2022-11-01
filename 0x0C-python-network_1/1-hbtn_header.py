9c6b108a25bce037b510f6e7088089622614856a
chostler24 <connor.hostler@gmail.com>
2022-11-01 12:09:45 -0500

    removed urllib

--- 0x0C-python-network_1/1-hbtn_header.py
+++ 0x0C-python-network_1/1-hbtn_header.py
@@ -4,12 +4,11 @@ Script that takes in a URL, sends request
 to the URL, and displays value of X-Request-Id
 variable in header of response.
 """
-import urllib
 from urllib import request
 import sys
 
 
 if __name__ == "__main__":
-    r = urllib.request.Request(sys.argv[1])
-    with urllib.request.urlopen(r) as response:
+    r = request.Request(sys.argv[1])
+    with request.urlopen(r) as response:
         print(response.info()['X-Request-Id'])
