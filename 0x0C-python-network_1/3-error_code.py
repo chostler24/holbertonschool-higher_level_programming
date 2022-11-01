d40f9355abe878db44820dbb0d516b8f62db33ca
chostler24 <connor.hostler@gmail.com>
2022-11-01 13:52:15 -0500

    task 3

---
+++ 0x0C-python-network_1/3-error_code.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Script takes in URL, sends request to URL and
+displays body of response
+"""
+from asyncore import read
+import urllib.request
+import urllib.error
+import sys
+
+
+if __name__ == "__main__":
+    URL = sys.argv[1]
+    r = urllib.request.Request(URL)
+    try:
+        with urllib.request.urlopen(r) as response:
+            print(response,read().decode('utf-8'))
+    except urllib.error.HTTPError as err:
+        print("Error code: {}".format(err.code))
