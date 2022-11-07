158b1a894f9dd4ff9768bcca266bb0e3501e800e
chostler24 <connor.hostler@gmail.com>
2022-11-07 09:44:31 -0600

    added if check

--- 0x0C-python-network_1/8-json_api.py
+++ 0x0C-python-network_1/8-json_api.py
@@ -1,4 +1,17 @@
 #!/usr/bin/python3
 """
-Script
+Script takes in a letter and sends a POST
+request to URL with letter as parameter
 """
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    URL = "http://0.0.0.0:5000/search_user"
+    if len(sys.argv) == 1:
+        q = ""
+    else:
+        q = sys.argv[1]
+
+
