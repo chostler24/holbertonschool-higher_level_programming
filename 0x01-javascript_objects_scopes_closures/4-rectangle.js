ec9fd57b4a8373a8a2914153e866262be2d5fa37
chostler24 <connor.hostler@gmail.com>
2022-12-02 13:00:38 -0600

    actually committing correct task now

--- 0x01-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x01-javascript_objects_scopes_closures/4-rectangle.js
@@ -12,6 +12,17 @@ class Rectangle {
       console.log('X'.repeat(this.width));
     }
   }
+
+  rotate () {
+    const somevar = this.width;
+    this.width = this.height;
+    this.height = somevar;
+  }
+
+  double () {
+    this.width = this.height * 2;
+    this.height = this.height * 2;
+  }
 }
 
 module.exports = Rectangle;
